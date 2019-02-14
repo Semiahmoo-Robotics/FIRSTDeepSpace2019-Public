19a305806de65609f2482d35d2fb44ee89a00a41
Dumble-Berries <777issoawesome@gmail.com>
2019-02-13 22:04:14 -0800

    renamed everything so its not troll anymore xD
    k so i fixed all the memery

--- src/main/java/frc/robot/subsystems/UltrasonicSensor.java
+++ src/main/java/frc/robot/subsystems/UltrasonicSensor.java
@@ -4,18 +4,17 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-//if you want you can change the names, I was just honestly bored and needed entertainment
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 
-public class HiMyNameIsBarryAllenAndImTheFastestManAlive {
-  private static final AnalogInput runninginthe90s = new AnalogInput(0);
-  private static final double omgSoMuchMath = 1.0;
-  public static double theFlash() {
-    return runninginthe90s.getVoltage();
+public class UltrasonicSensor {
+  private static final AnalogInput ultrasonicsensor = new AnalogInput(0);
+  private static final double volts_to_distance = 1.0;
+  public static double GetVoltage() {
+    return ultrasonicsensor.getVoltage();
   }
-  public static double getMeDatRuler() {
-    return theFlash() * omgSoMuchMath;
+  public static double getDistance() {
+    return GetVoltage() * volts_to_distance;
   }
 }
